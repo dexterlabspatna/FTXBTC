4b83151125d0234af32266ddc7cd3140dd221dbc
dexterlabspatna <88551522+dexterlabspatna@users.noreply.github.com>
2021-12-19 16:02:54 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, jsonify
+import json
+import keys
+import FTXClass
+import os
+app = Flask(__name__)
+
+c = FTXClass.FtxClient(api_key=keys.API_KEY, api_secret=keys.API_SECRET_KEY)
+
+@app.route("/welcome")
+def welcome():
+    print("WELCOME")
+    return "<p>welcome</p>"
+
+@app.route('/log', methods=['POST'])
+def log():
+    print(request.data)
+    return "<p>log</p>"
+	
+@app.route('/BTC', methods=['POST'])
+def webhook():
+    print(request.data)
+    data = json.loads(request.data)
+    result = c.place_order("BTC-PERP", data["transaction_type"])
+    print(result)
+    return{
+        "code": "error",
+        "message": "order"
+    }
---
+++ keys.py
@@ -0,0 +1,4 @@
+
+API_KEY = "CHO6ejGGJEAhaWpj0uH0jfDDllbW77jRwZIMjiHV"
+API_SECRET_KEY = "xhFR9zqBAskGns8WwhbjnXaSdsZWcKz_Z4TmNTDW"
+# ====================================================
